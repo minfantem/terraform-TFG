74ed3297523dfca306a875e0a1a0242d41dfb9d0
Mario Infante <marioinfantem01@gmail.com>
2024-06-14 15:58:55 +0000

    Eliminar todos los archivos del repositorio

--- s3.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_s3_bucket" "pantera_bucket" {
-  bucket = local.s3-sufix
-}
--- scripts/userdata.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Esto es un mensaje" > ~/mensaje.txt
-yum update -y
-yum install httpd -y
-systemctl enable httpd
-systemctl start httpd
